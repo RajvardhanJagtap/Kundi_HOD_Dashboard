91fdb0d4018ac68eb0a88b31a03e022c72c3fdb3
Jdamour12 <jeandamourkubwimana0@gmail.com>
2026-01-09 11:56:57 +0200

    Changed the name of base url variable to match the one used in the backend

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 // Helper functions for cookie management
 const getCookie = (name: string): string | null => {
