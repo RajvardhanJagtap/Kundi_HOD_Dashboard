63a04d5ca4d29d8dd02ce6c769b214c8c55a3f82
Jdamour12 <jeandamourkubwimana0@gmail.com>
2026-01-09 13:12:38 +0200

    Added a fallback base url

--- lib/api.ts
+++ lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://41.186.188.74:2000/api/v1';
 
 // Helper functions for cookie management
 const getCookie = (name: string): string | null => {
